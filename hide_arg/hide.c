444e4c01f374a0c3deff971d995d841da24e92a9
crendelyok <nikita.pankov.v@gmail.com>
2021-10-20 03:14:50 +0300

    hide-final

--- hide_arg/hide.c
+++ hide_arg/hide.c
@@ -1,15 +1,34 @@
 #include <sys/prctl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+
+#define PAGE_SHIFT 12
+#define PAGE_SIZE  (1UL << PAGE_SHIFT)
+#define PAGE_MASK  (~(PAGE_SIZE-1))
+#define PAGE_ALIGN(addr) (((addr)+PAGE_SIZE-1)&PAGE_MASK)
+
 int main(int argc, char* argv[], char* envp[])
 {
-	char* new_argv [argc-1];
-	for (int i = 0; i < argc-1; ++i) {
-		new_argv[i] = argv[i+1];
-		printf("%s\n", new_argv[i]);
+	long l = 0;
+	for (int i = 1; i < argc; ++i)
+		l += strlen(argv[i]);
+		
+	size_t nn_size = PAGE_ALIGN(l+1);
+	char* nn = mmap(NULL, nn_size, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+
+	unsigned long offset = 0;
+	for (int i = 1; i < argc; ++i) {
+		strncpy(nn + offset, argv[i], strlen(argv[i]));
+		offset += strlen(argv[i]);
 	}
-	prctl(PR_SET_MM, PR_SET_MM_ARG_START, new_argv, 0, 0);
-	while(1){sleep(1);};	
+	if (prctl(PR_SET_MM, PR_SET_MM_ARG_START, (unsigned long) nn, 0, 0) == -1) {
+		printf("1 prctl err");
+		return -1;
+	}
+	int i = 0;
+	while(1){i++;};	
 	
 	return 0;
 }
